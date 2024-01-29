67cd0e8887573fab4ba114a308a78b8ec0f955d4
Ngoc Thoa <luongdangngocthoa010595@gmail.com>
2024-01-29 10:51:36 +0700

    Create Project Synonis2nd

---
+++ wp-content/themes/synonis2nd/block-dev/webpack.config.js
@@ -0,0 +1,15 @@
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
+
+module.exports = {
+  //先頭で取得した既存の設定をスプレッド構文（...）で以下に展開
+  ...defaultConfig,
+  // 既存の設定を上書き
+  entry: {
+    "wdl-block-01": "./src/wdl-block-01.js",
+  },
+  output: {
+    path: path.join(__dirname, "../custom-blocks/build"),
+    filename: "[name].js",
+  },
+};
