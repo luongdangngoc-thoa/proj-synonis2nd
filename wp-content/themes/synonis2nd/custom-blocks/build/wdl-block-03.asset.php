ddb085277cd6d119ef5dc96ba8b49ab499272b94
luongnt <hienluong1997@gmail.com>
2024-01-30 15:45:47 +0700

    Delete unused file

--- wp-content/themes/synonis2nd/custom-blocks/build/wdl-block-03.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'a88260c35e0b20570e8b');
--- wp-content/themes/synonis2nd/custom-blocks/build/wdl-block-05.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '2e438d1e0ab09e408da3');
